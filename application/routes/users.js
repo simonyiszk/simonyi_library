26f0e6c6ebe3a2834c86b2df77a7770baca36361
Szabolcs Varadi <kresshy@gmail.com>
2014-11-23 22:56:02 +0100

    added json examples for communication and return user by id

--- application/routes/users.js
+++ application/routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var _profile = express.Router();
 var _userById = express.Router();
 
+var User = require('../models/user');
+
 /* GET user. */
 _profile.get('/', function(req, res) {
   res.send('respond with a resource');
@@ -13,11 +15,13 @@ _profile.get('/', function(req, res) {
  *
  */
 _userById.get('/', function(req, res) {
-	res.send('respond with a resource');
+  User.findOne(req.params.id, function(err, result) {
+    res.send(result);
+  });
 });
 
 /* export the routers */
 module.exports = {
-	profile: _profile,
-	userById: _userById
+  profile: _profile,
+  userById: _userById
 };
